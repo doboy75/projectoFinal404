2a1b2d7ba4b84d7da9df2b89d2531290ecb39c2b
Zezinho Luis <zezinholuis075@gmail.com>
2026-03-06 11:14:27 +0100

    Navbar e HomePage adicionados

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar></NavBar>
+  <HomePageFasco></HomePageFasco>
+
+
+
+  <router-view/>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HomePageFasco from './components/HomePageFasco.vue'
+import NavBar from './components/NavBar.vue';
+
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+NavBar,HomePageFasco
   }
 }
 </script>
@@ -21,6 +29,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav class="navbar navbar-expand-lg   px-4  margemButtom">
+<div class="container d-flex justify-content-between  px-5" id="navegacao">
+    <RouterLink to="" class="navbar-brand fs-2 fw-bold ">FASCO</RouterLink>
+<ul class="navbar-nav align-items-center gap-4">
+    <li class="nav-item"><RouterLink to="" class="nav-link">Home</RouterLink></li>
+      <li class="nav-item"><RouterLink to="" class="nav-link">Deals</RouterLink></li>
+        <li class="nav-item"><RouterLink to="" class="nav-link">New Arrivals</RouterLink></li>
+          <li class="nav-item"><RouterLink to="" class="nav-link">Packages</RouterLink></li>
+            <li class="nav-item"><RouterLink to="" class="nav-link">Sign in </RouterLink></li>
+             <li class="nav-item"><button class="btn btn-dark px-4 py-2 shadow">Sign up</button></li>
+</ul>
+</div>
+</nav>
+  <router-view/>
+</template>
+
+<style scoped>
+.margemButtom{
+
+  margin-bottom: 56px;
+}
+
+</style>
