6c3906bbd84fa2edd6bc50cdf26f1bef927b5925
Zezinho Luis <zezinholuis075@gmail.com>
2026-03-20 21:35:39 +0100

    nav produtos adicionado

--- src/components/ProdutosFasco.vue
+++ src/components/ProdutosFasco.vue
@@ -1,5 +1,23 @@
 <template>
-    
+    <nav class="navbar navbar-expand-lg    mb-5 ">
+<div class="container d-flex justify-content-between  px-5" id="navegacao">
+    <RouterLink to="" class="navbar-brand fs-2 fw-bold ">FASCO</RouterLink>
+<ul class="navbar-nav align-items-center gap-4">
+    <li class="nav-item"><RouterLink to="" class="nav-link custom-hove">Home</RouterLink></li>
+      <li class="nav-item"><RouterLink to="" class="nav-link custom-hove">Shop</RouterLink></li>
+        <li class="nav-item"><RouterLink to="" class="nav-link custom-hove">Products</RouterLink></li> 
+        <li class="nav-item"><RouterLink to="" class="nav-link custom-hove">Pages</RouterLink></li>     
+</ul>
+
+<div class="d-flex gap-3 justify-content-center align-items-center">
+    <img src="../assets/fashion/Vector (2).svg" alt="">
+    <img src="../assets/fashion/Vector (3).svg" alt="">
+    <img src="../assets/fashion/Vector (4).svg" alt="">
+    <img src="../assets/fashion/Vector (5).svg" alt="">
+
+</div>
+</div>
+</nav>
 <div class="container py-5">
     <div class="row">
         <div class="col-md-6 d-flex">
